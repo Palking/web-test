8fa44085dca0273a2721275d18cabdfcdce45d08
Palking <blah.olaf@web.de>
2016-12-24 12:11:12 +0100

    adding in projects page, expandable textContainers
    title + some small things

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,8 +1,26 @@
 var checker = true;
-//load templates
 $("document").ready(function(){
+    
+    //load templates
     $("#SideBar").load("template/sidebar.html");
     $("#NavBar").load("template/header.html"); 
+    
+    //Expandable textContainers
+    $(".textContainer").click(function(){
+        //alert("works");
+        if( $(this).hasClass("expanded")){
+        $(this)
+            .children("p")
+                .slideUp();
+        $(this).removeClass("expanded");
+        }
+        else{
+        $(this)
+            .children("p")
+                .slideDown();
+        $(this).addClass("expanded");
+        }
+    });
 });
 
 
