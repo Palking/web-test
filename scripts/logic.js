3bb9ed284de4d1b9b6ed54373bc6fa649a5b2a1a
Palking <blah.olaf@web.de>
2016-12-25 10:27:44 +0100

    first gitignore and textContainer stuff
    super messy in the current form. dont sync to master in current version!

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,4 +1,3 @@
-var checker = true;
 $("document").ready(function(){
     
     //load templates
@@ -22,17 +21,4 @@ $("document").ready(function(){
         }
     });
 });
-
-function DoStuff() {
-//    var field = document.getElementById("MainHead");
-//    field.innerHTML ="nice";
-    $("#MainHead").text("jQuery ftw");
-    if(checker){
-        $("#BigBox").fadeTo("slow", 0.33);
-        checker = false;
-    }
-    else{
-        $("#BigBox").fadeTo("slow", 1)
-        checker = true;
-    }
-}
+$("document").
