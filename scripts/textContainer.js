32984c99cda46c21978f7d231197cc9ca3492741
Palking <blah.olaf@web.de>
2016-12-26 18:33:26 +0100

    textContainer improvements
    added head and content divs.
    no templates yet

--- scripts/textContainer.js
+++ scripts/textContainer.js
@@ -1,1 +1,33 @@
-$(document).ready()
+$("document").ready(function(){
+    //Expandable textContainers
+    $(".textContainer>.head").click(function(){
+        //alert("works");
+        if( $(this).hasClass("expanded")){
+        $(this)
+            .siblings(".content")
+                .slideUp("fast");
+        $(this).removeClass("expanded");
+        $(this)
+            //is this good or bad practice
+            //.css({
+            //    borderBottomLeftRadius: 0, 
+            //    borderBottomRightRadius: 0 })
+            .animate({ 
+                borderBottomLeftRadius: 10, 
+                borderBottomRightRadius: 10}, 300);
+        }
+        else{
+        $(this)
+          //  .css({
+          //      borderBottomLeftRadius: 10, 
+          //      borderBottomRightRadius: 10 })
+            .animate({ 
+                borderBottomLeftRadius: 0, 
+                borderBottomRightRadius: 0}, 300);
+        $(this)
+            .siblings(".content")
+                .slideDown("fast");
+        $(this).addClass("expanded");
+        }
+    });
+});
