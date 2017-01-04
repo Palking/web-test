564ff3a321925df486385fcc29caed190c6ebf0c
Palking <blah.olaf@web.de>
2017-01-04 13:09:25 +0100

    improvments (info etc)

--- scripts/textContainer.js
+++ scripts/textContainer.js
@@ -1,33 +1,40 @@
 $("document").ready(function(){
     //Expandable textContainers
     $(".textContainer>.head").click(function(){
-        //alert("works");
-        if( $(this).hasClass("expanded")){
-        $(this)
+        //alert("head clicked");
+        var currentHead = $(this);
+        if( currentHead.hasClass("expanded")){
+        currentHead
             .siblings(".content")
-                .slideUp("fast");
-        $(this).removeClass("expanded");
-        $(this)
-            //is this good or bad practice
-            //.css({
-            //    borderBottomLeftRadius: 0, 
-            //    borderBottomRightRadius: 0 })
-            .animate({ 
+                .slideUp("fast", function(){
+                    //animate here -> waits for slideUp to finish
+                    
+                    currentHead
+                        .animate({ 
                 borderBottomLeftRadius: 10, 
-                borderBottomRightRadius: 10}, 300);
-        }
+                borderBottomRightRadius: 10}, 100);
+                })
+                currentHead.removeClass("expanded")
+                    .children(".containerInfo")
+                        .html("[click to expand]");
+         }
         else{
-        $(this)
+        currentHead
           //  .css({
           //      borderBottomLeftRadius: 10, 
           //      borderBottomRightRadius: 10 })
             .animate({ 
                 borderBottomLeftRadius: 0, 
-                borderBottomRightRadius: 0}, 300);
-        $(this)
-            .siblings(".content")
-                .slideDown("fast");
-        $(this).addClass("expanded");
+                borderBottomRightRadius: 0}, 100,
+                function(){
+                    currentHead
+                        .siblings(".content")
+                            .slideDown("fast")
+                    currentHead
+                        .addClass("expanded")
+                        .children(".containerInfo")
+                            .html("[click to colapse]");                  
+                });
         }
     });
 });
